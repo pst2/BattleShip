0ffb40a72651f7e685b6dcf1900f3f7b80590c46
Phan Sơn Thịnh <23020229@vnu.edu.vn>
2025-04-10 17:21:26 +0700

    Update Battle Ships update with sdl.cpp

--- Battle Ships update with sdl.cpp
+++ Battle Ships update with sdl.cpp
@@ -4,17 +4,18 @@
 #include <cstdlib>
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <chrono>
 
 using namespace std;
 
 const int BOARD_SIZE = 10;
 const int CELL_SIZE = 50;
+const int SCREEN_HEIGHT = BOARD_SIZE * CELL_SIZE + 100;//+100 để hiện thị thêm bộ đếm trên màn hình
 const int SCREEN_WIDTH = BOARD_SIZE * CELL_SIZE;
-const int SCREEN_HEIGHT = BOARD_SIZE * CELL_SIZE;
-const int NUM_SHIPS = 3; 
-const int MAX_ATTEMPTS = 30; 
-const int TIME_LIMIT = 60;
+const int NUM_SHIPS = 3;
+const int MAX_ATTEMPTS = 36;
+const int TIME_LIMIT = 90;
 
 struct Ship {
     int x, y;
@@ -22,12 +23,11 @@ struct Ship {
     bool horizontal;
 };
 
-vector<vector<char>> board(BOARD_SIZE, vector<char>(BOARD_SIZE, '~')); 
-vector<Ship> ships; 
-int attempts = 0; 
-int hits = 0; 
+vector<vector<char>> board(BOARD_SIZE, vector<char>(BOARD_SIZE, '~'));
+vector<Ship> ships;
+int attempts = 0;
+int hits = 0;
 
-//render
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer* ren) {
     SDL_Texture* texture = IMG_LoadTexture(ren, file.c_str());
     if (!texture) {
@@ -35,26 +35,60 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer* ren) {
     }
     return texture;
 }
-
-void renderBoard(SDL_Renderer* renderer) {
+bool isShipSunk(const Ship& ship) {
+    for (int i = 0; i < ship.length; ++i) {
+        int x = ship.x + (ship.horizontal ? 0 : i);
+        int y = ship.y + (ship.horizontal ? i : 0);
+        if (board[x][y] != 'X') return false;
+    }
+    return true;
+}
+//Tạo bảng
+void renderBoard(SDL_Renderer* renderer, SDL_Texture* shipImage) {
     for (int i = 0; i < BOARD_SIZE; ++i) {
         for (int j = 0; j < BOARD_SIZE; ++j) {
             SDL_Rect cell = {j * CELL_SIZE, i * CELL_SIZE, CELL_SIZE, CELL_SIZE};
             if (board[i][j] == 'X') {
-                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255); 
+                SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+                SDL_RenderFillRect(renderer, &cell);
+
+                SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+                SDL_RenderDrawRect(renderer, &cell);
             } else if (board[i][j] == 'O') {
-                SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); 
+                SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+                SDL_RenderFillRect(renderer, &cell);
             } else {
-                SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255); 
+                SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+                SDL_RenderFillRect(renderer, &cell);
             }
-            SDL_RenderFillRect(renderer, &cell);
-            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); 
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderDrawRect(renderer, &cell);
         }
     }
-}
 
-//đặt thuyền
+    for (const auto& ship : ships) {
+        if (isShipSunk(ship)) {
+            for (int i = 0; i < ship.length; ++i) {
+                int x = ship.x + (ship.horizontal ? 0 : i);
+                int y = ship.y + (ship.horizontal ? i : 0);
+                SDL_Rect dst = {y * CELL_SIZE, x * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+                SDL_RenderCopy(renderer, shipImage, NULL, &dst);
+            }
+        }
+    }
+}
+//Vị trí tàu
+void revealShips(SDL_Renderer* renderer, SDL_Texture* shipImage) {
+    for (const auto& ship : ships) {
+        for (int i = 0; i < ship.length; ++i) {
+            int x = ship.x + (ship.horizontal ? 0 : i);
+            int y = ship.y + (ship.horizontal ? i : 0);
+            SDL_Rect dst = {y * CELL_SIZE, x * CELL_SIZE, CELL_SIZE, CELL_SIZE};
+            SDL_RenderCopy(renderer, shipImage, NULL, &dst);
+        }
+    }
+}
+//Đặt ví trí cho tàu
 bool isValidPlacement(const Ship& ship) {
     if (ship.horizontal) {
         if (ship.y + ship.length > BOARD_SIZE) return false;
@@ -69,7 +103,6 @@ bool isValidPlacement(const Ship& ship) {
     }
     return true;
 }
-
 void placeShip(const Ship& ship) {
     if (ship.horizontal) {
         for (int i = 0; i < ship.length; ++i) {
@@ -82,148 +115,182 @@ void placeShip(const Ship& ship) {
     }
     ships.push_back(ship);
 }
-
 void placeShips() {
     srand(time(0));
     for (int i = 0; i < NUM_SHIPS; ++i) {
         Ship ship;
-        ship.length = rand() % 2 + 2; 
+        ship.length = rand() % 2 + 2;
         ship.horizontal = rand() % 2 == 0;
-
         do {
             ship.x = rand() % BOARD_SIZE;
             ship.y = rand() % BOARD_SIZE;
         } while (!isValidPlacement(ship));
-
         placeShip(ship);
     }
 }
-//trúng
-bool isHit(int x, int y) {
-    for (const auto& ship : ships) {
-        if (ship.horizontal) {
-            if (ship.y <= y && y < ship.y + ship.length && ship.x == x) return true;
-        } else {
-            if (ship.x <= x && x < ship.x + ship.length && ship.y == y) return true;
+//Render chữ trên màn hình
+void renderText(SDL_Renderer* renderer, TTF_Font* font, const string& text, int x, int y) {
+    SDL_Color color = {255, 255, 255, 255};
+    SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect dstRect = {x, y, surface->w, surface->h};
+    SDL_FreeSurface(surface);
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+    SDL_DestroyTexture(texture);
+}
+void renderCenterText(SDL_Renderer* renderer, TTF_Font* font, const string& text) {
+    SDL_Color color = {255, 255, 255, 255};
+    SDL_Surface* surface = TTF_RenderText_Solid(font, text.c_str(), color);
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect dstRect = {SCREEN_WIDTH/ 2 - surface->w / 2, SCREEN_HEIGHT/ 2 - surface->h / 2, surface->w, surface->h};
+    SDL_FreeSurface(surface);
+    SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+    SDL_DestroyTexture(texture);
+}
+//Bắt đầu trò chơi
+void waitForKeyToStart(SDL_Renderer* renderer, TTF_Font* font) {
+    bool waiting = true;
+    SDL_Event event;
+    while (waiting) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) exit(0);
+            else if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_p) {
+                waiting = false;
+            }
         }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        renderCenterText(renderer, font, "Press P to start.");
+        SDL_RenderPresent(renderer);
     }
-    return false;
 }
 
 int main(int argc, char* argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        cout << "SDL không thể khởi tạo: " << SDL_GetError() << endl;
-        return -1;
-    }
-    if (!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
-        cout << "SDL_image không thể khởi tạo: " << IMG_GetError() << endl;
-        return -1;
-    }
-
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+    TTF_Init();
+//Tạo cửa sổ, đồ họa, font chữ
     SDL_Window* window = SDL_CreateWindow("Battleship", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-   //render đồ họa
     SDL_Texture* background = loadTexture("board.png", renderer);
-    if (!background) {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
+    SDL_Texture* winImage = loadTexture("Congratulation! You win!.png", renderer);
+    SDL_Texture* lostImage = loadTexture("You lost.png", renderer);
+    SDL_Texture* TimesoutImage = loadTexture("Times out!.png", renderer);
+    SDL_Texture* shipImage = loadTexture("tàu 2.jpg", renderer);
+
+    TTF_Font* font = TTF_OpenFont("arial.ttf", 24);
+    if (!font) {
+        cout << "Can't reload font: " << TTF_GetError() << endl;
         return -1;
     }
 
-     SDL_Texture* winImage = loadTexture("Congratulation! You win!.png", renderer);
-     if (!winImage) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return -1;
-     }
-     SDL_Texture* lostImage = loadTexture("You lost.png", renderer);
-     if (!lostImage) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return -1;
-     }
-     SDL_Texture* TimesoutImage = loadTexture("Times out!.png", renderer);
-     if (!TimesoutImage) {
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return -1;
-     }
+    while (true) {
+        board = vector<vector<char>>(BOARD_SIZE, vector<char>(BOARD_SIZE, '~'));
+        ships.clear(); attempts = 0; hits = 0;
 
+        waitForKeyToStart(renderer, font);
+        placeShips();
+//Bộ đếm thời gian
+        auto start_time = chrono::high_resolution_clock::now();
+        bool running = true;
+        SDL_Event event;
 
-    placeShips();
+        while (running) {
+            auto current_time = chrono::high_resolution_clock::now();
+            int elapsed_time = chrono::duration_cast<chrono::seconds>(current_time - start_time).count();
 
-    bool running = true;
-    SDL_Event event;
+            if (elapsed_time >= TIME_LIMIT) {
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, TimesoutImage, NULL, NULL);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(3000);
 
-//đếm thời gian
-    auto start_time = std::chrono::high_resolution_clock::now();
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, background, NULL, NULL);
+                renderBoard(renderer, shipImage);
+                revealShips(renderer, shipImage);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(5000);
 
-    while (running) {
-        auto current_time = std::chrono::high_resolution_clock::now();
-        auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count();
+                break;
+            }
 
-        if (elapsed_time >= TIME_LIMIT) {
-            cout << "Times out! You lost." << endl;
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) return 0;
+                else if (event.type == SDL_MOUSEBUTTONDOWN) {
+                    int x = event.button.y / CELL_SIZE;
+                    int y = event.button.x / CELL_SIZE;
+                    if (board[x][y] == '~') { board[x][y] = 'O'; attempts++; }
+                    else if (board[x][y] == 'S') { board[x][y] = 'X'; attempts++; hits++; }
+                }
+            }
+
+            if (attempts >= MAX_ATTEMPTS) {
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, lostImage, NULL, NULL);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(3000);
+
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, background, NULL, NULL);
+                renderBoard(renderer, shipImage);
+                revealShips(renderer, shipImage);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(5000);
+
+                break;
+            }
+
+            if (hits >= NUM_SHIPS * 2) {
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, winImage, NULL, NULL);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(3000);
+
+                SDL_RenderClear(renderer);
+                SDL_RenderCopy(renderer, background, NULL, NULL);
+                renderBoard(renderer, shipImage);
+                revealShips(renderer, shipImage);
+                SDL_RenderPresent(renderer);
+                SDL_Delay(5000);
+
+                break;
+            }
+
             SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, TimesoutImage, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(5000);
-            running = false;
-        }
+            SDL_RenderCopy(renderer, background, NULL, NULL);
+            renderBoard(renderer, shipImage);
+//Hiển thị bố đếm thời gian và lượt chơi trên màn hình
+            string timeText = "Time left: " + to_string(TIME_LIMIT - elapsed_time) + "s";
+            string attemptText = "Attempts left: " + to_string(MAX_ATTEMPTS - attempts);
+            renderText(renderer, font, timeText, 10, SCREEN_HEIGHT - 90);
+            renderText(renderer, font, attemptText, 10, SCREEN_HEIGHT - 50);
 
-        while (SDL_PollEvent(&event)) {
-    if (event.type == SDL_QUIT) running = false;
-    else if (event.type == SDL_MOUSEBUTTONDOWN) {
-        int x = event.button.y / CELL_SIZE;
-        int y = event.button.x / CELL_SIZE;
-
-        if (board[x][y] == '~') { 
-            board[x][y] = 'O'; 
-            cout << "Missed (" << x << ", " << y << ")!" << endl;
-            attempts++;
-        } else if (board[x][y] == 'S') { 
-            board[x][y] = 'X'; 
-            cout << "Correct (" << x << ", " << y << ")!" << endl;
-            hits++;
-            attempts++;
-        }
-    }
-}
-        if (attempts >= MAX_ATTEMPTS) {
-            cout << "Times play out! You lost." << endl;
-            SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, lostImage, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(5000);
-            running = false;
-        }
-
-        //thắng
-        if (hits >= NUM_SHIPS * 2) { 
-            cout << "Congratulation! You win" << endl;
-            SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, winImage, NULL, NULL);
-    SDL_RenderPresent(renderer);
-    SDL_Delay(5000);
-            running = false;
+            SDL_RenderPresent(renderer);
         }
 
         SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer, background, NULL, NULL); 
-        renderBoard(renderer); 
+        SDL_RenderCopy(renderer, background, NULL, NULL);
+        renderCenterText(renderer, font, "Press R to reset or Q to exit.");
         SDL_RenderPresent(renderer);
+
+        bool wait = true;
+        while (wait) {
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_QUIT) wait = false;
+                else if (event.type == SDL_KEYDOWN) {
+                    if (event.key.keysym.sym == SDLK_q) return 0;
+                    else if (event.key.keysym.sym == SDLK_r) wait = false;
+                }
+            }
+        }
     }
 
-    SDL_DestroyTexture(background);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     IMG_Quit();
+    TTF_Quit();
     SDL_Quit();
     return 0;
 }
-
